ac74fe385dbe3dbac5b0332a4229ec8eb29b74b7
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-02 18:47:09 +0330

    in progress

--- src/App.js
+++ src/App.js
@@ -4,3 +4,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './Character'
 import { COPY_Fail, COPY_SUCCESS } from './message';
+
+const App () => {
+    
+}
