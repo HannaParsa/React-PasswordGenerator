3db8761767d6099d392a5e99b289f213b8dff29b
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-02 23:26:44 +0330

    in progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./App.css"
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './Character'
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify'
 import { COPY_Fail, COPY_SUCCESS } from './message';
 
-const App () => {
+
+const App = () => {
     const [password, setPassword] = useState("")
     const [passwordLength, setPasswordLength] = useState(26)
     const [includeUpperCase, setIncludesUpperCase] = useState(false)
     const [includeLowerCase, setIncludeLowerCase] = useState(false)
-    const [incluseNumbers, setIncludeNumbers] = useState(false)
+    const [includeNumbers, setIncludeNumbers] = useState(false)
     const [includeSymbols, setIncludeSymbols] = useState(false)
     
     const handleGeneratePassword = () => {
-        if(!incluseNumbers && !includeUpperCase && !includeLowerCase && !includeSymbols){
-            notify("you must choose of the options at least", true)
+        if(!includeNumbers && !includeUpperCase && !includeLowerCase && !includeSymbols){
+            //notify("you must choose of the options at least", true)
         }
         else{
            let characterList = ""
@@ -23,9 +24,19 @@ const App () => {
                characterList = characterList + numbers
            }
            if (includeUpperCase){
-               characterList = characterList + includeUpperCase
+               characterList = characterList + upperCaseLetters
            }
-
+           if (includeLowerCase){
+               characterList = characterList + lowerCaseLetters
+           }
+           if (includeSymbols){
+               characterList = characterList + specialCharacters
+           }
+           setPassword(creatPassword(characterList))
+           //notify("password generated successfully", false)
         }
+    }
+    const creatPassword = (characterList) => {
+
     }
 }
