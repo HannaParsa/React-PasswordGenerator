a7626d22f721c01a4be9bdbf4e9e488fbe7f32fb
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-03 22:01:02 +0330

    in progress

--- src/App.js
+++ src/App.js
@@ -50,10 +50,18 @@ const App = () => {
     }
     const notify = (message, hasError = false) => {
         if(hasError){
-
+            toast.error(message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
         }
         else {
-            
+
         }
     }
 }
