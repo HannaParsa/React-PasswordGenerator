085c5b1ffc868590784bb940549cc2769a43c911
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-03 21:51:12 +0330

    in progress

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
     const characterListLength = characterList.length
     for (let i = 0 ; i < passwordLength ; i ++){
         const characterIndex = Math.round(Math.random() * characterListLength)
-        
+        password = password +  characterList.charAt(characterIndex)
     }
     }
 }
