9b0be6a16d7a8d1d03957a86ca8280e21e398d5c
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-06-04 20:54:12 +0330

    in progress

--- src/App.js
+++ src/App.js
@@ -61,7 +61,15 @@ const App = () => {
             });
         }
         else {
-
+             toast(message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+             });
         }
     }
 }
